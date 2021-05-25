1acb3cda7157aa4ece46954d804e7a127aacdaa1
D. Sinitsyn <dmitry.sinitsyn@2lemetry.ru>
2021-05-25 21:43:54 +0300

    Junk, junk, and junk

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,56 @@
-import {App, addIcon, Notice, parseLinktext, Plugin, PluginSettingTab, Setting} from 'obsidian';
-import { SampleSettingTab} from "./settings";
-
-const PLUGIN_CSS_CLASS_NAME = 'highlight-related-link'
-
-interface MyPluginSettings {
-    mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-    mySetting: 'default'
-}
+import {addIcon, Notice, parseLinktext, Plugin} from 'obsidian';
+import {
+    DEFAULT_SETTINGS,
+    HighlighterSettingTab,
+    LinkHighlighterSettings,
+} from "./settings";
+import {Colorizer} from "./helpers";
+import {PLUGIN_CSS_CLASS_NAME} from "./constants";
 
 export default class MyPlugin extends Plugin {
     coloredElements: any[] = [];
     enabled: boolean = true;
-    settings: MyPluginSettings;
+    settings: LinkHighlighterSettings;
+    colorizer: Colorizer;
 
     async onload() {
-
-    	// TODO: move icon to separate file?
         addIcon('highlighter', '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="highlighter" class="svg-inline--fa fa-highlighter fa-w-17" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 544 512"><path fill="currentColor" d="M0 479.98L99.92 512l35.45-35.45-67.04-67.04L0 479.98zm124.61-240.01a36.592 36.592 0 0 0-10.79 38.1l13.05 42.83-50.93 50.94 96.23 96.23 50.86-50.86 42.74 13.08c13.73 4.2 28.65-.01 38.15-10.78l35.55-41.64-173.34-173.34-41.52 35.44zm403.31-160.7l-63.2-63.2c-20.49-20.49-53.38-21.52-75.12-2.35L190.55 183.68l169.77 169.78L530.27 154.4c19.18-21.74 18.15-54.63-2.35-75.13z"/></svg>')
 
-        this.addRibbonIcon('highlighter', 'Switch Links Highlight', () => {
+        await this.loadSettings().then(() => {
+            this.colorizer = new Colorizer(this.settings);
+            this.colorizer.setupColors();
+        })
+
+        this.addRibbonIcon('highlighter', 'Toggle Links Highlighter', () => {
             if (this.enabled) {
-                new Notice('Highlighter disabled.');
-                this.enabled = false
-				this.cleanLinkHighlighter()
+                new Notice('Links Highlighter disabled.');
+                this.cleanLinkHighlighter()
             } else {
-                new Notice('Highlighter enabled!');
-                this.enabled = true
-				this.checkEnabled()
+                new Notice('Links Highlighter enabled!');
+                this.checkEnabled()
             }
+            this.enabled = !this.enabled
         });
 
-        this.app.metadataCache.on('changed', () => {
-            console.error('changed')
-            this.checkEnabled()
-        })
+        this.app.metadataCache.on('changed', () => this.checkEnabled())
+        this.app.workspace.on('file-open', () => this.checkEnabled())
 
-        this.app.workspace.on('file-open', () => {
-            console.error('file-open')
-            this.checkEnabled()
-        })
-        //
-        // this.app.workspace.on('layout-change', () => {
-        //     console.error('layout-change')
-        //     this.checkEnabled()
-        // })
+        this.app.workspace.on('css-change', () => this.colorizer.setupColors())
 
-        this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.addSettingTab(new HighlighterSettingTab(this.app, this));
     }
 
-	onunload() {
-		this.cleanLinkHighlighter()
-	}
+    onunload() {
+        this.cleanLinkHighlighter()
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
     checkEnabled() {
         if (this.enabled) {
@@ -64,10 +61,8 @@ export default class MyPlugin extends Plugin {
 
     findLinks() {
         let activeFile = this.app.workspace.getActiveFile();
-        // console.log('activeFile', activeFile)
         if (activeFile) {
             let fileCache = this.app.metadataCache.getFileCache(activeFile)
-            // console.log('fileCache', fileCache)
             if (fileCache && fileCache.links) {
                 fileCache.links.forEach((link) => {
                     let linkpath = this.app.metadataCache.getFirstLinkpathDest(parseLinktext(link.link).path, '/');
@@ -86,13 +81,5 @@ export default class MyPlugin extends Plugin {
     cleanLinkHighlighter() {
         this.coloredElements.forEach((el) => el.classList.remove(PLUGIN_CSS_CLASS_NAME))
         this.coloredElements = []
-    }
-
-    async loadSettings() {
-        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    }
-
-    async saveSettings() {
-        await this.saveData(this.settings);
     }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,68 +1,90 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
 import MyPlugin from "./main";
+import {Colorizer} from "./helpers";
 
-const DEFAULT_BACKGROUND_COLOR = 'var(--text-muted)';
-const DEFAULT_COLOR = 'var(--interactive-accent)';
-const EXAMPLE_BUTTON_CLASSNAME = 'link-highlighter-example';
+export interface LinkHighlighterSettings {
+    useCustomColors: boolean,
+    customBackgroundColor: string,
+    customTextColor: string
+}
 
-export class SampleSettingTab extends PluginSettingTab {
+export const DEFAULT_SETTINGS: LinkHighlighterSettings = {
+    useCustomColors: false,
+    customBackgroundColor: '#ffffff',
+    customTextColor: '#000000'
+}
+
+export class HighlighterSettingTab extends PluginSettingTab {
     plugin: MyPlugin;
-
-    backgroundColorValue = DEFAULT_BACKGROUND_COLOR;
-    colorValue = DEFAULT_COLOR
-
-    defaultButtonValues: string = `{
-        background-color: ${this.backgroundColorValue}!important;
-        color: var(--meow-meow)!important;
-    }`
+    colorizer: Colorizer;
 
     constructor(app: App, plugin: MyPlugin) {
         super(app, plugin);
         this.plugin = plugin;
+        this.colorizer = new Colorizer(this.plugin.settings)
     }
 
     display(): void {
         let {containerEl} = this;
 
-        let meowmeow = '--meow-meow';
-
-        document.body.style.setProperty(meowmeow, 'green');
-
-        let stylesheet = document.styleSheets[0]
-
-        let ruleindex = stylesheet.insertRule(`.${EXAMPLE_BUTTON_CLASSNAME} ${this.defaultButtonValues}`)
-        console.log(stylesheet.rules[ruleindex])
-        // stylesheet.rules.item().
-
         containerEl.empty();
-        containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+        containerEl.createEl('h2', {text: 'Link Highlighter Settings'});
 
         new Setting(containerEl)
-            .setName('Background Color (hex code)')
-            .addText(text => text
-                .setPlaceholder('Enter your secret')
-                .setValue('')
+            .setName('Use Custom Colors')
+            .addToggle(component => {
+                component
+                    .setValue(this.plugin.settings.useCustomColors)
+                    .onChange(async (enabled) => {
+                        this.plugin.settings.useCustomColors = enabled
+                        await this.plugin.saveSettings()
+
+                        if (enabled) {
+                            this.colorizer.setTextColor(this.plugin.settings.customTextColor)
+                            this.colorizer.setBackgroundColor(this.plugin.settings.customBackgroundColor)
+                            toggleInputDisable(false)
+                        } else {
+                            toggleInputDisable(true)
+                            this.colorizer.setupColors()
+                        }
+                    })
+            })
+
+        let backgroundColorSetting = new Setting(containerEl)
+            .setName('Custom Background Color')
+            .addText(component => component
+                .setPlaceholder('Enter HEX color code')
+                .setValue(this.plugin.settings.customBackgroundColor)
+                .setDisabled(!this.plugin.settings.useCustomColors)
                 .onChange(async (value) => {
-                    console.log(value)
-                    document.body.style.setProperty(meowmeow, value);
-                    // this.plugin.settings.mySetting = value;
-                    // await this.plugin.saveSettings();
+                    this.colorizer.setBackgroundColor(value);
+                    this.plugin.settings.customBackgroundColor = value;
+                    await this.plugin.saveSettings();
                 }));
 
-        new Setting(containerEl).setName('Element highlighting example').addButton(btn =>
-            btn
-                .setClass(EXAMPLE_BUTTON_CLASSNAME)
-                .setButtonText('Zаshtshееshtshауоushtshееkhsуа')
-                .setDisabled(true)
-        )
+        let textColorSetting = new Setting(containerEl)
+            .setName('Custom Text Color')
+            .addText(text => text
+                .setPlaceholder('Enter HEX color code')
+                .setValue(this.plugin.settings.customTextColor)
+                .onChange(async (value) => {
+                    this.colorizer.setTextColor(value);
+                    this.plugin.settings.customTextColor = value;
+                    await this.plugin.saveSettings();
+                }));
 
-        // new Setting(containerEl).setName('Abother One').addButton(btn =>
-        //     btn
-        //         .setButtonText('change css').onClick(() => {
-        //         this.test2 = this.test1
-        //         document.styleSheets[0].insertRule(`.${this.testClass} { background-color: orange!important }`)
-        //     })
-        // )
-        // containerEl.createDiv({ text: 'ExampleDiv'}, (el) => el.classList.add(this.testClass))
+        new Setting(containerEl)
+            .setName('Element highlighting example')
+            .addButton(btn =>
+                btn
+                    .setClass('link-highlighter-example')
+                    .setButtonText('Hello World!')
+                    .setDisabled(true)
+            )
+
+        function toggleInputDisable(value: boolean) {
+            backgroundColorSetting.setDisabled(value)
+            textColorSetting.setDisabled(value)
+        }
     }
 }
